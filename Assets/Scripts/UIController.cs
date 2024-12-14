f199d8b1d8e43208680084ecce06912b61749cd2
DeniskaWolk <nedelkowolk@gmail.com>
2024-12-14 21:10:32 +1000

    added structures

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -6,22 +6,23 @@ using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    public Action OnRoadPlacement, OnHousePlacement, OnWindmillPlacement;
-    public Button placeRoadButton, placeHouseButton, placeWindmillButton;
+    public Action showBuildPanelButton, OnRoadPlacement, OnHousePlacement, OnWindmillPlacement, OnApiaryPlacement, OnShopPlacement, OnElectricGeneratorPlacement;
+    public Button placeRoadButton, placeHouseButton, placeWindmillButton, placeApiaryButton, placeShopButton, placeElectricGeneratorButton;
 
     public Color outlineColor;
     List<Button> buttonList;
 
+    public Animator animator;
+
     private void Start()
     {
-        buttonList = new List<Button> { placeHouseButton, placeRoadButton, placeWindmillButton };
+        buttonList = new List<Button> { placeHouseButton, placeRoadButton, placeWindmillButton, placeApiaryButton, placeShopButton, placeElectricGeneratorButton };
 
         placeRoadButton.onClick.AddListener(() =>
         {
             ResetButtonColor();
             ModifyOutline(placeRoadButton);
             OnRoadPlacement?.Invoke();
-
         });
         placeHouseButton.onClick.AddListener(() =>
         {
@@ -35,9 +36,32 @@ public class UIController : MonoBehaviour
             ResetButtonColor();
             ModifyOutline(placeWindmillButton);
             OnWindmillPlacement?.Invoke();
-
+        });
+        placeApiaryButton.onClick.AddListener(() =>
+        {
+            ResetButtonColor();
+            ModifyOutline(placeApiaryButton);
+            OnApiaryPlacement?.Invoke();
+        });
+        placeShopButton.onClick.AddListener(() =>
+        {
+            ResetButtonColor();
+            ModifyOutline(placeShopButton);
+            OnShopPlacement?.Invoke();
+        });
+        placeElectricGeneratorButton.onClick.AddListener(() =>
+        {
+            ResetButtonColor();
+            ModifyOutline(placeElectricGeneratorButton);
+            OnElectricGeneratorPlacement?.Invoke();
+        });
+        showBuildPanelButton.onClick.AddListener(() =>
+        {
+            bool tempBool = animator.GetBool("isShow");
+            animator.SetBool("isShow", !tempBool);
         });
     }
+
 
     private void ModifyOutline(Button button)
     {
